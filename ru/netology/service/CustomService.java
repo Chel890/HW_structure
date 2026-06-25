318f9326f74fb17ed0b8b0fdc0b6a5441df82393
Tapka <142623401+Chel890@users.noreply.github.com>
2026-06-25 07:22:08 +0300

    Update CustomService.java

--- ru/netology/service/CustomService.java
+++ ru/netology/service/CustomService.java
@@ -1,8 +1,9 @@
 package ru.netology.service;
 
 public class CustomService {
-    public static final int CALCULATE_CASTOM(int price, int weight) {
-        int result = (price/100) + (weight*100);
+    public static final int WEIGHT_RATE = 100;
+    public static final int calculateCastom(int price, int weight) {
+        int result = (price/100) + (WEIGHT_RATE*weight);
         return result;
     }
 }
